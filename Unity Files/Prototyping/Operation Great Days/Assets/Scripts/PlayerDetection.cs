c05525304a8b353501d7509bdaa7c8e993268e03
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2020-11-12 12:19:04 +0000

    Unity Prototype progress
    Created unity file
    Created Camera detection zone

---
+++ Unity Files/Prototyping/Operation Great Days/Assets/Scripts/PlayerDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+      
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("TARGET IN SIGHT");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("LOST TARGET");
+        }
+    }
+}
---
+++ Unity Files/Prototyping/Operation Great Days/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private Rigidbody playerRGDB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRGDB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float lateral = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, lateral);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            playerRGDB.AddForce(movement * (speed/2));
+        }
+        else
+        {
+            playerRGDB.AddForce((movement * speed)/ Time.deltaTime);
+        }
+        
+    }
+}
